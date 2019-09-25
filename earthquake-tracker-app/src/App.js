57a53104fcbe28aa14787fc0b58cb648aa120e55
German Nunez <germannunez@Germans-MacBook-Pro.local>
2019-09-25 13:05:16 -0700

    comp did mount

--- earthquake-tracker-app/src/App.js
+++ earthquake-tracker-app/src/App.js
@@ -6,10 +6,17 @@ class App extends React.Component{
     earthquakes: []
   }
 
+  async componentDidMount(){
+    console.log('comp did mount')
+    const allQuakes = await this.getEarthQuakes()
+    this.setState({
+      earthquakes: allQuakes
+    })
+  }
+  
   getEarthQuakes = async () => {
     try{
       const quakes = await fetch('http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson')
-      console.log(quakes)
 
       if(!quakes.ok){
         throw Error(Response.statusText)
@@ -17,14 +24,14 @@ class App extends React.Component{
 
       const quakesJson = await quakes.json()
       return quakesJson.features
-      
+
     }catch(err){
       console.log(err)
     }
   }
 
   render(){
-    console.log(this.getEarthQuakes())
+    console.log(this.state.earthquakes)
     return (
       <div className="App">
         <h1>heyy</h1>
